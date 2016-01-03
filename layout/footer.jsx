2c055e46f43b3884c83961cbd005c0cdcf32b97a
Yonas Berhe <yonas.berhe007@gmail.com>
2016-01-02 18:35:24 -0800

    no dummy content anymore

---
+++ layout/footer.jsx
@@ -0,0 +1,9 @@
+Footer = React.createClass({
+  render() {
+    render (
+      <div className="footer">
+        <p> This is the footer.</p>
+      </div>
+    )
+  }
+});
---
+++ layout/header.jsx
@@ -0,0 +1,12 @@
+Header = React.createClass ({
+  render() {
+    return (
+      <div className="header">
+        <p>
+          This is the header.
+        </p>
+
+      </div>
+    )
+  }
+})
